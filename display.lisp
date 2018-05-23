10649e43dcddfc6eaafd82119277f31c74ddfca5
kchen29 <kchen29@stuy.edu>
2018-05-22 21:34:55 -0400

    more fast

--- display.lisp
+++ display.lisp
@@ -1,23 +1,17 @@
 ;;;; Interacting with screens. Primarily interfacing to the outside.
 
-(defparameter *screen-side* 0)
-(defparameter *screen* 0)
-(defparameter *z-buffer* 0)
-
-(defun make-screen (side)
-  (setf *screen-side* side)
-  (let ((dimensions (list side side)))
-    (setf *screen* (make-array dimensions :initial-element '(0 0 0)))
-    (setf *z-buffer* (make-array dimensions :initial-element most-negative-double-float))))
-
-(make-screen 500)
+(defconstant +screen-side+ 500)
+(defparameter *screen* (make-array '(500 500) :initial-element '(0 0 0)))
+(defparameter *z-buffer* (make-array '(500 500) :initial-element most-negative-double-float))
+(declaim (type (simple-array list (500 500)) *screen*)
+         (type (simple-array double-float (500 500)) *z-buffer*))
 
 (defun plot (x y z color)
   "Plots (x, y) on *SCREEN* with COLOR. Checks bounds.
    COLOR is not copied. Checks the z-value with *z-buffer*."
   ;;round to 3 decimal places
   (setf z (fround-to z 3))
-  (when (and (< -1 x *screen-side*) (< -1 y *screen-side*)
+  (when (and (< -1 x +screen-side+) (< -1 y +screen-side+)
              (> z (aref *z-buffer* x y)))
     (setf (aref *screen* x y) color
           (aref *z-buffer* x y) z)))
@@ -31,11 +25,14 @@
 
 (defun print-screen ()
   "Prints *SCREEN* to standard out."
-  (format t "P3 ~a ~a 255" *screen-side* *screen-side*)
-  (loop for y from (1- *screen-side*) downto 0
-        do (progn (format t "~%")
-                  (loop for x below *screen-side*
-                        do (format t "~{~a ~}" (aref *screen* x y))))))
+  (format t "P3 ~a ~a 255" +screen-side+ +screen-side+)
+  (do ((y (1- +screen-side+) (1- y)))
+      ((< y 0))
+    (terpri)
+    (dotimes (x +screen-side+)
+      (dolist (c (aref *screen* x y))
+        (princ c)
+        (write-char #\ )))))
 
 (defun save (filename)
   "Saves *SCREEN* to filename.
@@ -49,8 +46,8 @@
 (defun clear-screen ()
   "Clears *SCREEN*. Sets all the pixels to black.
    Clears *Z-BUFFER*. Sets all the values to the least float."
-  (dotimes (x *screen-side*)
-    (dotimes (y *screen-side*)
+  (dotimes (x +screen-side+)
+    (dotimes (y +screen-side+)
       (setf (aref *screen* x y) '(0 0 0)
             (aref *z-buffer* x y) most-negative-double-float))))
 
